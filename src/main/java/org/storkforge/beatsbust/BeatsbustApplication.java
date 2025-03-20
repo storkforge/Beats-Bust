41aaf109f886c12a8cb8f1591d0d14c73828be6e
DeenoDev00 <drizzysha@gmail.com>
2025-03-20 13:10:49 +0100

    Initial Commit Spring boot setup

---
+++ src/main/java/org/storkforge/beatsbust/BeatsbustApplication.java
@@ -0,0 +1,13 @@
+package org.storkforge.beatsbust;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeatsbustApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeatsbustApplication.class, args);
+    }
+
+}
